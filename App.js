54544c6b90bf69e5bd748e0d1785b5eb3ba15ff6
Sundar Paul <53091076+sundarpaul@users.noreply.github.com>
2022-04-06 21:00:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
+import SearchIcon from "./SearchIcon.svg";
+import "./App.css";
+import Post from './components/post/post'
+
+const API_URL = "http://www.omdbapi.com/?apikey=b6003d8a";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>Tal Mentors</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for Mentors"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {/* {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Mentors found</h2>
+        </div>
+      )} */}
+
+    <div className="container">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+
+    </div>
+  );
+};
+
+export default App;
